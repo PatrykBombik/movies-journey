64a34febb109660061affd19832ecfad679ec309
PatrykBombik <patryk.bombik@gmail.com>
2023-08-08 15:44:24 +0300

    goToTop done

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,13 @@ window.addEventListener('scroll', function () {
     window.scrollY >= 10 ? header.classList.add('active') : header.classList.remove('active');
 
 });
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+    window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+})
